c4f311c6c59c342c369a1d9e2124d0392d4789d6
Tanisha Mathur <139479958+tanisham2@users.noreply.github.com>
2025-08-29 17:28:22 +0530

    Robot_returns.java

--- Robot_returns.java
+++ Robot_returns.java
@@ -10,3 +10,23 @@ class Solution {
         return x == 0 && y == 0;
     }
 }
+
+//using Switch
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            switch (moves.charAt(i)) {
+                case 'L': x--; 
+                    break;
+                case 'R': x++;
+                    break;
+                case 'U': y++;
+                    break;
+                case 'D': y--; 
+                    break;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+}
