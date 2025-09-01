f6b715062ee2c344313c118fb6818e06a4e284ac
Tanisha Mathur <139479958+tanisham2@users.noreply.github.com>
2025-09-01 16:03:19 +0530

    Single-Number.java

---
+++ Single-Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            result ^= num;       
+        }
+        return result;
+    }
+}
